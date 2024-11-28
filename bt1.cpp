cd755f524b19e4f137fe82f0e8a98bf76a003642
khanhh89 <khanhabcabc0000@gmail.com>
2024-11-29 00:38:27 +0700

    Add files via upload

---
+++ bt1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};
+
+    printf("Cac phan tu trong mang tu cuoi ve dau la:\n");
+    for (int i = 4; i >= 0; i--) { 
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
